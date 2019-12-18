d0bd2661b40aa49cd8e0c33e19cdb6562d72edec
Yasirurandeepa <yasirurandeepa.15@cse.mrt.ac.lk>
2019-12-18 20:38:20 +0530

    Initial commit

---
+++ src/main/java/com/example/rider/model/User.java
@@ -0,0 +1,76 @@
+package com.example.rider.model;
+
+import org.springframework.security.core.parameters.P;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User extends AuditModel {
+
+    public User() {
+
+    }
+
+    public User(String Username, String Email, String Phone){
+        this.username = Username;
+        this.email = Email;
+        this.phone = Phone;
+    }
+
+    @Id
+    @GeneratedValue(generator = "user_generator")
+    @SequenceGenerator(
+            name = "user_generator",
+            sequenceName = "user_sequence",
+            initialValue = 1
+    )
+    private Long id;
+
+    @NotBlank
+    @Size(min =1, max = 50)
+    private String username;
+
+    @NotBlank
+    @Column(columnDefinition = "text")
+    private String email;
+
+    @NotBlank
+    @Column(columnDefinition = "text")
+    private String phone;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/com/example/rider/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.rider.repository;
+
+import com.example.rider.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
